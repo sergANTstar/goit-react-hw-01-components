3dcf38c7e7aa03bacd8dfb95fea5edd9f413250b
sergANTstar <106014778+sergANTstar@users.noreply.github.com>
2022-09-08 20:07:02 +0300

    u

--- src/component/Statistics/Statistics.jsx
+++ src/component/Statistics/Statistics.jsx
@@ -17,5 +17,11 @@ export const Statistics = ({title, stats}) => {
 
 Statistics.propTypes = {
     title: PropTypes.string,
-   stats: PropTypes.array,
-};
+    stats: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          label: PropTypes.string.isRequired,
+          percentage: PropTypes.number.isRequired,
+        }),
+    ),
+}
